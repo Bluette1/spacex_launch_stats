dd9b678464555b837e39f0fcd9e492c09d3142ae
Bluette1 <marylene.sawyer@gmail.com>
2021-04-03 05:58:48 +0200

    Finished react appollo client

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const schema = require('./schema');
 
 // Allow cross-origin
@@ -13,6 +14,11 @@ app.use(
     graphiql: true,
   })
 );
+
+app.use(express.static('public'));
+app.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => `Server running on port ${PORT}`);
